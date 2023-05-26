4d7ddc73c18af8d815e54d99117c2a208cb73a94
Magomed <magamedvisitaev8@gmail.com>
2023-05-26 22:01:32 +0600

    .

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss {
+    private int health;
+    private int damage;
+    private String protection;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getProtection() {
+        return protection;
+    }
+
+    public void setProtection(String protection) {
+        this.protection = protection;
+    }
 }
--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Hero {
+public class Hero {
+    private int health;
+    private int damage;
+    private String superpower;
+
+    public Hero(int health, int damage, String superpower) {
+        this.health = health;
+        this.damage = damage;
+        this.superpower = superpower;
+    }
+
+    public Hero(int health, int damage) {
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getSuperpower() {
+        return superpower;
+    }
+
+    public void setSuperpower(String superpower) {
+        this.superpower = superpower;
+    }
 }
